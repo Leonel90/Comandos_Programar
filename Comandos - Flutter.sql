c2d70bb1e6fb7bc644395d91da6e15540ebf7797
unknown <sleonel1990@gmail.com>
2023-10-26 12:26:05 -0500

    flutter

---
+++ Comandos - Flutter.sql
@@ -0,0 +1,22 @@
+/* Insatalar de preferencia Visual Code Rosado como administrador */
+seleccionamos 
+-->Desarrollo para escritorio con C++
+-->Desarrollo Móvil con C++
+
+1.- descargar Flutter y copiar la carpeta al disco C: ahi lo dejamos
+
+2.- Copiamos la direccion o ruta de ubicacion y vamos a variables de entorno
+C:\flutter\bin en los dos path
+
+3.- en el Simbolo del sistema o cmd ----> flutter --version
+
+4.- flutter doctor
+
+5.- Instalamos los plugins necesarios
+
+6.- flutter --version
+
+7.- Creamos un nuevo proyecto ubicandonos en la carpeta donde queremos ---> flutter create app1
+
+lib--> main.dart --> archivo importante mustra la estructura q tiene nuestro proyecto
+pubspec.yaml---> es donde vamos agregando o cargando los plugins o extenciones necesarios poniendro los link de lo q necesitamos.
