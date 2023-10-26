f97df5837efc87ca2761e72e3921519c4323ce24
unknown <sleonel1990@gmail.com>
2023-10-26 12:45:31 -0500

    Comandos NestJS

---
+++ Comandos NestJS.sql
@@ -0,0 +1,17 @@
+/*Descargamos he instalamos Laragon*/
+
+/* Instalamos el cliente de NestJS*/
+    npm i -g @nestjs/cli
+
+/* Creamos un nuevo proyecto*/
+    nest new "Nombre proyecto"
+       escojes ----> npm
+
+/* Dentro del proyecto instalamos las librerias de Typeorm */
+    npm i --save @nestjs/typeorm typeorm
+
+/* Levantamos el proyecto o los servicios por lotes y q automaticamnte se actualice los cambios */
+    npm run start
+    npm run start:dev
+
+/* */
